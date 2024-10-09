ed58d0687711d722c44d0bdadceeacea2ae83ec1
local-interloper <marinoljubetic8@gmail.com>
2024-10-09 23:17:51 +0200

    Trying to turn this into a nix package

--- flake.nix
+++ flake.nix
@@ -2,20 +2,43 @@
   description = "A dev enviornment for my game of life clone";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/24.05";
   };
 
-  outputs = { self, nixpkgs, ... }: 
-  let 
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in 
-  {
-    devShells.${system}.default = pkgs.mkShell {
-      inputsFrom = with pkgs; [
-        gcc
-        cmake
-      ];
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          pkg-config
+          SDL2
+          SDL2_ttf
+        ];
+      };
+
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        name = "game-of-life";
+        src = ./.;
+
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          gcc
+          SDL2
+          SDL2_ttf
+        ];
+
+        buildPhase = ''
+          gcc src/*.c -I src/include -o game-of-life $(pkg-config --cflags --libs sdl2 SDL2_ttf)
+        '';
+
+        installPhase = ''
+          mkdir -p $out/bin
+          cp game-of-life $out/bin
+          cp -r assets $out/bin
+        '';
+      };
     };
-  };
 }
